1d405ef7d3fdcd14c77670c73b559797c12ae721
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-05-04 16:58:09 +1200

    Generate output into `docs` directory to suite GitHub pages.

--- bake/utopia/project.rb
+++ bake/utopia/project.rb
@@ -19,7 +19,14 @@ end
 def static(output_path: "docs")
 	require 'rackula/command'
 	
+	config_path = File.expand_path("../../template/config.ru", __dir__)
 	public_path = File.expand_path("../../public", __dir__)
 	
-	Rackula::Command::Top["generate", "--force", "--public", public_path, "--output-path", output_path].call
+	Rackula::Command::Top["generate", "--force",
+		"--config", config_path,
+		"--public", public_path,
+		"--output-path", output_path
+	].call
+	
+	FileUtils.touch File.expand_path(".nojekyll", output_path)
 end
