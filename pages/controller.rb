0570bdebbdda3665af5ca7137a098c3d25d9238d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-05-05 03:51:14 +1200

    Add support for image header.

--- pages/controller.rb
+++ pages/controller.rb
@@ -7,10 +7,6 @@ on 'index' do
 	if readme_path = @base.path_for('README.md')
 		@document = Kramdown::Document.new(File.read(readme_path), syntax_highlighter: nil)
 		
-		if title = @document.root.children.shift
-			@title = title.children.first.value
-		end
-		
 		start = @document.root.children.index{|node| node.children.first&.value == "Usage"}
 		finish = start + 1
 		
